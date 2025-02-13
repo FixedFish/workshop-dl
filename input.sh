b09a82f4b0c87daf137bdbb8e046141274ed6c72
FixedFish <fish.raw@rambler.ru>
2025-02-13 14:40:50 +0500

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=1281930
-export ENTRY_ID=2439880423
+export ENTRY_ID=3407770135
 
 # tmod file to extract using extractTmod.yml workflow
 export TMODFILE="EquipAllAccessory_v0.0.0.3.tmod"
