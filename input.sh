9cbc418492d1c91defa4523b454307580e40ce7f
FixedFish <fish.raw@rambler.ru>
2025-02-13 14:55:29 +0500

    Update input.sh

--- input.sh
+++ input.sh
@@ -4,4 +4,4 @@ export APP_ID=1281930
 export ENTRY_ID=3407770135
 
 # tmod file to extract using extractTmod.yml workflow
-export TMODFILE="EquipAllAccessory_v0.0.0.3.tmod"
+export TMODFILE="BossTeams.tmod"
