1f371dde67daf14af73729d14d1bda4b84110256
FixedFish <fish.raw@rambler.ru>
2025-02-15 14:36:49 +0500

    Update input.sh

--- input.sh
+++ input.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 105600 for Terraria, 1281930 for TModLoader
 export APP_ID=1281930
-export ENTRY_ID=3407770135
+export ENTRY_ID=2992213994
 
 # tmod file to extract using extractTmod.yml workflow
 export TMODFILE="BossTeams.tmod"
